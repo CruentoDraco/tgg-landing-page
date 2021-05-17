52d34a5166762b3d9f667106973c76c8174a8419
CruentoDraco <re-deubzer@t-online.de>
2021-05-17 14:21:39 +0200

    complete webpage overhaul

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,20 @@
 function setPage(){
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const fromApp = urlParams.get('fromApp');
-    switch (fromApp) {
-        case "instagram":
-           setLink("Instagram"); 
+    const site = urlParams.get('site');
+    switch(site){
+        case "home":
+            document.getElementById("view").setAttribute("src", "includes/home.html");
+            document.getElementById("view").style.height= "600px";
+            $("#about").removeClass("active");
+            $("#home").addClass("active");
         break;
-        case "twitter":
-            setLink("Twitter");
-        break;
-        case "twitch":
-            setLink("Twitch");
-        break;
-        case "youtube":
-            setLink("Youtube");    
-        break;
-        default:
-            setLink("");
+        case "about":
+            document.getElementById("view").setAttribute("src", "includes/about.html");
+            document.getElementById("view").style.height= "970px";
+            $("#home").removeClass("active");
+            $("#about").addClass("active");
         break;
     }
-}
 
-function setLink(platform){
-    htmlLinkBtn = "";
-    if(platform != ""){
-        htmlLinkBtn =  '<a href="https://www.links.cruentodraco.de/?style=tgg&fromApp='+ String.toLowerCase(platform) +'&homepage=true" class="btn btn-outline btn-block" role="button" target="_blank"><i class="fas fa-link">&nbsp;</i>Cooler Stuff auch Links genannt</a>'
-    } else{
-        htmlLinkBtn =  '<a href="https://www.links.cruentodraco.de/?style=tgg&homepage=true" class="btn btn-outline btn-block" role="button" target="_blank"><i class="fas fa-link">&nbsp;</i>Cooler Stuff auch Links genannt</a>'
-    }
-    document.getElementById("link-btn").innerHTML = htmlLinkBtn;
 }
-
