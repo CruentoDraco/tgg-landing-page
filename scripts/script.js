6de1b5b8507cbce655dd5f7715380d4621ce811c
CruentoDraco <re-deubzer@t-online.de>
2021-05-17 15:17:30 +0200

    fixed iframe

--- scripts/script.js
+++ scripts/script.js
@@ -6,12 +6,14 @@ function setPage(){
         case "home":
             document.getElementById("view").setAttribute("src", "includes/home.html");
             document.getElementById("view").style.height= "600px";
+            $("#view").addClass("home");
             $("#about").removeClass("active");
             $("#home").addClass("active");
         break;
         case "about":
             document.getElementById("view").setAttribute("src", "includes/about.html");
             document.getElementById("view").style.height= "970px";
+            $("#view").addClass("about");
             $("#home").removeClass("active");
             $("#about").addClass("active");
         break;
