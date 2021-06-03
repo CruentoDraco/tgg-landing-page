bb4aa90b9a6296230448faa22db5704de7d305dd
CruentoDraco <re-deubzer@t-online.de>
2021-06-03 17:54:05 +0200

    design overhaul

--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,18 @@ function setPage(){
     const site = urlParams.get('site');
     switch(site){
         case "home":
-            document.getElementById("view").setAttribute("src", "includes/home.html");
-            document.getElementById("view").style.height= "1090px";
-            $("#view").removeClass("about");
-            $("#view").addClass("home");
+            $('#view').load('includes/home.html');
             $("#about").removeClass("active");
             $("#home").addClass("active");
         break;
         case "about":
-            document.getElementById("view").setAttribute("src", "includes/about.html");
-            document.getElementById("view").style.height= "970px";
-            $("#view").removeClass("home");
-            $("#view").addClass("about");
+            $('#view').load('includes/about.html');
             $("#home").removeClass("active");
             $("#about").addClass("active");
+        break;
+        default:
+            $('#view').load('includes/home.html');
+            $("#home").addClass("active");
         break;
     }
 
