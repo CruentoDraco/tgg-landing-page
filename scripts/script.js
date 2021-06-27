490e7ede35338eb15d73aaad6632c996f40afdf4
CruentoDraco <re-deubzer@t-online.de>
2021-06-28 00:07:23 +0200

    Design overhaul; added Branding Site

--- scripts/script.js
+++ scripts/script.js
@@ -6,12 +6,20 @@ function setPage(){
         case "home":
             $('#view').load('includes/home.html');
             $("#about").removeClass("active");
+            $("#branding").removeClass("active");
             $("#home").addClass("active");
         break;
         case "about":
             $('#view').load('includes/about.html');
             $("#home").removeClass("active");
+            $("#branding").removeClass("active");
             $("#about").addClass("active");
+        break;
+        case "branding":
+            $('#view').load('includes/branding.html');
+            $("#home").removeClass("active");
+            $("#about").removeClass("active");
+            $("#branding").addClass("active");
         break;
         default:
             $('#view').load('includes/home.html');
